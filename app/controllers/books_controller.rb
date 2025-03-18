8a1586ae04e6215e00e57376bc138a84e91fff7c
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-03-18 17:23:58 +0100

    books show page is almost done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
-
   def show
+    @book = Book.find(params[:id])
+    @reviews = @book.reviews
+    authorize @book
   end
 
   def search
   end
-  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
 end
