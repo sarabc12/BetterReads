c22d28806c7d75b60cb429b905ff984fcd29ad66
Adam Smyth <adamss.smyth@gmail.com>
2025-03-18 16:23:24 +0000

    search bar v1, still need to make changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = policy_scope(Book)
+  end
+
   def show
+    authorize @book
   end
 
   def search
+    authorize Book
+
+    if params[:query].present?
+      @books = policy_scope(Book).where("title ILIKE ? OR author ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @books = policy_scope(Book)
+    end
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :books do
+    collection do
+      get 'search'
+    end
+  end
 end
