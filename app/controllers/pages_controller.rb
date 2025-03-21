ffb33e0dfbfe664d3b36ae66722f53f3b78e6682
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-03-21 16:00:29 +0100

    new css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @news = News.order('RANDOM()').limit(5)
+    @news = News.order('RANDOM()').limit(4)
   end
 end
